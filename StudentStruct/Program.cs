de8ef8ed2776ea2bc12063b1678607348f2a7172
Dell <Dell@ZISHAN>
2021-09-14 18:30:17 +0530

    Add project files.

---
+++ StudentStruct/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace StudentStruct
+{
+    public struct Student
+    {
+        public string name;
+        public string course;
+        public int stdId;
+        public int regId;
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1;
+            Console.WriteLine("Enter Student Details");
+            Console.Write("Enter Student Name: ");
+            s1.name = Console.ReadLine();
+            Console.Write("Enter Course: ");
+            s1.course = Console.ReadLine();
+            Console.Write("Enter Student_ID: ");
+            s1.stdId =Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Student Registration_ID: ");
+            s1.regId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("The Student Details are\n");
+            Console.WriteLine("\tName= {0}\n\tCourse= {1}\n\tStudentID= {2}\n\tRegistrationID= {3}\n", s1.name, s1.course, s1.stdId, s1.regId);
+           // Console.WriteLine("Enter Student Details");
+            /*Student s2 = s1;
+            Console.Write("\tEnter Student Name: ");
+            s2.name = Console.ReadLine();
+            Console.Write("\tEnter Student_ID: ");
+            s2.stdId = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\tEnter Student Registration_ID: ");
+            s2.regId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Second Student Details are\n");
+            Console.WriteLine("Name= {0}\nCourse= {1}\nStudentID= {2} \nRegistrationID= {3} ", s2.name, s2.course, s2.stdId, s2.regId);*/
+        }
+    }
+}
